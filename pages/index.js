be9ec4a011a8ff43a521029e83c73c7eceaf1e3a
beverm2391 <beverman4528@gmail.com>
2023-01-07 04:09:07 -0500

    fix package lock

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ export default function Index() {
       <div>
         <h1 className='text-6xl font-bold'>Hello world!</h1>
         <div className='mt-8'>
+          <p className='text-gray-600 dark:text-gray-400'>
+            This is a sample website using Next.js and Tailwind CSS.
+          </p>
         </div>
       </div>
     </Container>
